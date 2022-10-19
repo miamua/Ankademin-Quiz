a274f0af0133dc6be9107cedfb73b994961a887e
miamua <nanthiporn.aumaim@gmail.com>
2022-10-19 22:22:35 +0200

    added multiple choices/checkbox function

--- index.js
+++ index.js
@@ -1,81 +1,159 @@
 let questionsArr = [
     {
         question: "1. Is Vim the best editor?",
-        answers: "true"
+        answers: ["true", "false"],
+        correct: ["true"],
+        type: "radio"
     },
     {
         question: "2. Is Nano the best editor?",
-        answers: "false"
+        answers: ["Yes", "No", "Never", "Sometimes"],
+        correct: ["Yes", "Sometimes"],
+        type: "checkbox"
     },
     {
         question: "3. Is Brandon the best teacher?",
-        answers: "true"
+        answers: ["true", "false", "maybe", "never"],
+        correct: ["true"],
+        type: "radio"
     },
     {
         question: "4. Is Nackademin the best school on earth?",
-        answers: "true"
+        answers: ["true", "true", "true", "false"],
+        correct: ["true", "true", "true"],
+        type: "checkbox"
     },
     {
         question: "5. Is the earth flat?",
-        answers: "false"
+        answers: ["true", "true", "true", "false"],
+        correct: ["true", "true", "true"],
+        type: "checkbox"
     },
     {
         question: "6. 10x10 = 1000?",
-        answers: "false"
+        answers: ["true", "false"],
+        correct: ["false"],
+        type: "radio"
     },
     {
         question: "7. 5x5 = 25?",
-        answers: "true"
+        answers: ["true", "false"],
+        correct: ["true"],
+        type: "radio"
     },
     {
         question: "8. Fish cannot blink?",
-        answers: "true"
+        answers: ["true", "false"],
+        correct: ["true"],
+        type: "radio"
     },
     {
         question: "9. An octopus has three hearts?",
-        answers: "true"
+        answers: ["true", "false"],
+        correct: ["true"],
+        type: "radio"
     },
     {
         question: "10. Is this the last question?",
-        answers: "true"
+        answers: ["true", "false"],
+        correct: ["true"],
+        type: "radio"
     },
 
 ];
 
 
-let questionNumber = 0;
+let questionNumber = 0
 let points = 0;
 let maxPoints = 10;
-let getResultBtn = document.querySelector("#submitAnswers");
-let questionDiv = document.querySelector("#questions-div")
-let questionLi = document.createElement("div");
-questionLi.innerText = questionsArr[questionNumber].question;
-questionDiv.append(questionLi);
+let questionBox = document.querySelector("#questionbox")
+let questionAnswer = document.querySelector("#questionAnswer")
+let question = document.querySelector("#question")
+let answerSubmit = document.createElement("button")
+let startQuiz = document.createElement("button")
+answerSubmit.textContent = "Submit"
+startQuiz.innerHTML = "Start Quiz"
+let endResults = [];
+questionBox.append(startQuiz)
 
 
-getResultBtn.addEventListener("click", () => {
-    let userAnswer = document.querySelector("[name='userAnswer']:checked").value;
-    if (userAnswer == questionsArr[questionNumber].answers) {
-        points++;
+startQuiz.addEventListener("click", () => {
+    question.innerText = questionsArr[questionNumber].question;
+    startQuiz.remove()
+    questionBox.append(answerSubmit)
+    for (i = 0; i < questionsArr[questionNumber].answers.length; i++) {
+        let radioButton = document.createElement("input")
+        radioButton.setAttribute("type", questionsArr[questionNumber].type);
+        radioButton.setAttribute("name", "answear");
+        radioButton.setAttribute("value", questionsArr[questionNumber].answers[i]);
+        radioButton.setAttribute("id", "answear" + i);
+        questionAnswer.appendChild(radioButton);
 
+        let radioLabel = document.createElement("label");
+        radioLabel.innerHTML = (questionsArr[questionNumber].answers[i])
+        questionAnswer.appendChild(radioLabel)
     }
 
-    if (questionNumber < questionsArr.length - 1) {
-        questionNumber++;
-        questionLi.innerText = questionsArr[questionNumber].question; //get next question
-        questionDiv.append(questionLi)
-    } else if (questionNumber === questionsArr.length - 1) {
-        console.log("Finished! Your score is " + points);
-        document.querySelector("#container").style.display = "none";
+});
+
+answerSubmit.addEventListener("click", () => {
+    let answered = false;
+    let answeredQuestions = [];
+    let answer = document.getElementsByName("answear");
+    let RightOrWrong = ""
+    for (i = 0; i < answer.length; i++) {
+        if (answer[i].checked) {
+            answered = true
+            answeredQuestions.push(answer[i].value)
+        }
+    }
+    console.log(questionsArr[questionNumber].correct)
+    console.log(answeredQuestions)
+    if (JSON.stringify(answeredQuestions) == JSON.stringify(questionsArr[questionNumber].correct)) {
+        console.log("Rätt")
+        RightOrWrong = "✅";
+        points++;
+    } else {
+        console.log("Wrong")
+        RightOrWrong = "❌";
+    }
+
+    let testToPutAll = { "question": questionsArr[questionNumber].question, "playerAns": answeredQuestions, "Result": RightOrWrong };
+    endResults.push(testToPutAll);
+
+    if (!answered) {
+        alert("Please select an answear")
+    } else if (questionNumber < questionsArr.length - 1) {
+
+        questionAnswer.innerHTML = ""
+
+        questionNumber++
+        question.innerText = questionsArr[questionNumber].question;
+
+        for (i = 0; i < questionsArr[questionNumber].answers.length; i++) {
+            radioButton = document.createElement("input")
+            radioButton.setAttribute("type", questionsArr[questionNumber].type);
+            radioButton.setAttribute("name", "answear");
+            radioButton.setAttribute("value", questionsArr[questionNumber].answers[i]);
+            radioButton.setAttribute("id", "answear" + i);
+            questionAnswer.appendChild(radioButton)
+
+            radioLabel = document.createElement("label");
+            radioLabel.innerHTML = (questionsArr[questionNumber].answers[i])
+            questionAnswer.appendChild(radioLabel)
+        }
+    } else {
+        document.querySelector("#questionbox").style.display = "none";
         let resultBtn = document.createElement("button");
         resultBtn.innerText = "Show score";
-        document.querySelector("#website-info").append(resultBtn);
+        document.querySelector("#resultBox").append(resultBtn);
 
 
         resultBtn.addEventListener("click", () => {
+            console.log("Finished! Your score is " + points);
             result = document.createElement("h2");
             result.innerText = "Finished! Your score is " + points;
-            document.querySelector("#website-info").append(result);
+            document.querySelector("#resultBox").append(result);
             resultBtn.style.display = "none";
             gradeText = document.createElement("h3");
 
@@ -92,16 +170,48 @@ getResultBtn.addEventListener("click", () => {
                 gradeText.innerText = "Underkänd!";
                 gradeText.style.color = "red";
             }
-            document.querySelector("#website-info").append(gradeText);
+            document.querySelector("#resultBox").append(gradeText);
+            getAllquestion()
+            console.log(endResults)
         })
 
     }
 
-    //remove a check from radio button
-    let uncheck = document.querySelector("[name='userAnswer']:checked");
-    uncheck.checked = false;
 
 })
+
+function getAllquestion() {
+    let scoreTable = document.createElement("table");
+    scoreTable.style.width = "100%";
+    let scoreRow = document.createElement("tr");
+    let allquestions = document.createElement("th");
+    let allUserAnswers = document.createElement("th");
+    let allCorrectAnswers = document.createElement("th");
+    document.querySelector("#allResults").append(scoreTable);
+    scoreTable.append(scoreRow);
+    allquestions.textContent = "Questions";
+    allCorrectAnswers.innerText = "Results";
+    allUserAnswers.innerText = "Your answers";
+    scoreRow.append(allquestions, allUserAnswers, allCorrectAnswers);
+
+
+
+    endResults.forEach((question) => {
+        let questionsRow = document.createElement("tr");
+        let questionsItem = document.createElement("td");
+        let userAnswerItem = document.createElement("td");
+        let ResultItem = document.createElement("td");
+        questionsItem.innerText = question.question;
+        userAnswerItem.innerText = question.playerAns;
+        ResultItem.innerText = question.Result;
+        document.querySelector("table").append(questionsRow);
+
+        questionsRow.append(questionsItem);
+        questionsRow.append(userAnswerItem);
+        questionsRow.append(ResultItem);
+    })
+};
+
 
 let darkModeBtn = document.querySelector("#darkMode");
 let darkMode = document.querySelector("body");
@@ -118,7 +228,9 @@ darkModeBtn.addEventListener("click", () => {
     lightModeBtn.style.display = "";
     darkModeBtn.style.display = "none";
 
-})
+
+
+});
 
 lightModeBtn.addEventListener("click", () => {
     lightMode.style.backgroundColor = "white";
@@ -126,4 +238,5 @@ lightModeBtn.addEventListener("click", () => {
     darkModeBtn.style.display = "";
     lightModeBtn.style.display = "none";
 
-})
+
+});
